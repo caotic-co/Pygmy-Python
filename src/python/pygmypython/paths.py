f0df8d91a019c6829c986b6cb1c8b9321c9fa2cf
Camilo <camilo.ospinaa@gmail.com>
2020-05-24 23:17:27 -0500

    Fix Join Python Path

--- src/python/pygmypython/paths.py
+++ src/python/pygmypython/paths.py
@@ -5,6 +5,6 @@ OPENJDK_PATH = os.path.abspath("openjdk")
 if environment.is_dev_environment():
     OPENJDK_PATH = os.path.abspath("dist/pygmypython/openjdk")
 
-JAVA = os.path.join(OPENJDK_PATH, "/bin/java")
-JAVAC = os.path.join(OPENJDK_PATH, "/bin/javc")
-JAR = os.path.join(OPENJDK_PATH, "/bin/jar")
+JAVA = os.path.join(OPENJDK_PATH, "bin/java")
+JAVAC = os.path.join(OPENJDK_PATH, "bin/javc")
+JAR = os.path.join(OPENJDK_PATH, "bin/jar")
