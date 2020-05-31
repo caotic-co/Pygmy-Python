735b90bc8d3ac134424c80731eae3e95f7503c98
Camilo Ospina <camilo.ospinaa@gmail.com>
2020-05-31 09:16:14 -0500

    Remove jvm installation

--- src/python/pygmypython/entry_point.py
+++ src/python/pygmypython/entry_point.py
@@ -1,14 +1,22 @@
 import subprocess
+from shutil import which
 from typing import List
 from typing import NewType
 from src.python.pygmypython import __version__
-from src.python.pygmypython.paths import JAVA
 
 ReturnCode = NewType("ReturnCode", int)
 
 
 def main(argv: List[str]) -> ReturnCode:
+    if not which("java"):
+        print("'java' is not available in the system path")
+        return ReturnCode(1)
+
+    if not which("javac"):
+        print("'javac' is not available in the system path")
+        return ReturnCode(1)
+
     print("Pygmy Python v{}".format(__version__))
     print("Argv: {}".format(argv))
-    print(subprocess.check_output([JAVA, "-version"], universal_newlines=True))
+    print(subprocess.check_output(["java", "-version"], universal_newlines=True))
     return ReturnCode(0)
